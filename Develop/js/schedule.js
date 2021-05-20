5594680cbf46d8236940e0ac52d2b840f71ab110
Frostquill <ruiz82494@gmail.com>
2021-05-19 18:53:21 -0700

    color code added

--- Develop/js/schedule.js
+++ Develop/js/schedule.js
@@ -1,9 +1,53 @@
 var events = {};
-var currentDay = $("#currentDay");
+
 
 var tOfDay= function(){
+var currentDay = $("#currentDay");
 var day = moment().format('MMMM Do YYYY')
 currentDay.text(day);
+
+};
+tOfDay();
+
+var hour = 9;
+var currentTime = moment().format("k");
+
+var colorCode = function() {
+
+$(".row").each(function( index ) {
+    var hours = moment()
+    hours = moment().set('hour', hour).format("ha");
+    
+    $(this).children(".hour").text(hours);
+   
+    if (hour < currentTime){
+        $(this).children(".event").removeClass("col col-xl-10 event present future past").addClass("col col-xl-10 event past");
+     } else if (hour > currentTime) {
+         
+         $(this).children(".event").removeClass("col col-xl-10 event present future past").addClass("col col-xl-10 event future");
+     } else {
+         
+         $(this).children(".event").removeClass("col col-xl-10 event present future past").addClass("col col-xl-10 event present");
+     }
+     
+     hour++
+});
+
+
+
+
 };
 
-tOfDay();
+var saveEvent = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  $(".event").onClick 
+  
+
+
+
+
+colorCode();
+
+
